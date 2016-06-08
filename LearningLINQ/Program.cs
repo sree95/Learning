09eea0e7d9ff2c3c9cc99c0f18d1c08ce1e31986
geeksree <sreenivasparimi95@live.in>
2016-06-09 01:35:05 +0530

    Adding LearningLINQ Solution to Repository

---
+++ LearningLINQ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Car> listoCars = new List<Car>()
+            {
+                new Car() { BrandName = "Tayota" , OwnerName = "Alex", Model = 1998},
+                new Car() { BrandName = "Honda" , OwnerName = "Phenix", Model = 2001},
+                new Car() { BrandName = "Hundai" , OwnerName = "JRD Tata", Model = 1889},
+                new Car() { BrandName = "Marithi" , OwnerName = "John", Model = 1999},
+                new Car() { BrandName = "Audi" , OwnerName = "Castelman", Model = 2005}
+            };
+
+            IEnumerable<Car> enumerable = from car in listoCars
+                                          select car;
+            foreach (var car in enumerable)
+            {
+                Console.WriteLine(car.OwnerName);
+            }
+        }
+    }
+
+    public class Car
+    {
+        public string BrandName { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public int Model { get; set; }
+    }
+}
