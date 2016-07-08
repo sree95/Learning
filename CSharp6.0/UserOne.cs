9c682a1c8aad20f9fe7a3bd6bc7d9284cd2fcfa0
sparimiEPAM <sparimi@HYD-SPARIMI01>
2016-07-08 20:54:09 +0530

    Adding CSharp6 project

---
+++ CSharp6.0/UserOne.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+
+namespace CSharp6._0
+{
+    public class UserOne
+    {
+        // Before C# 6.0
+
+        private readonly Guid newGuid = Guid.NewGuid();
+
+        private string _userName = string.Empty;
+
+
+        public Guid Id
+        {
+            get
+            {
+                return newGuid;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            protected set
+            {
+                _userName = value;
+            }
+        }
+
+        public static Guid _id { get; } = Guid.NewGuid(); 
+      
+
+        public string _userNameOne { get; protected set; } = string.Empty;
+
+
+        // Auto Property Initializers
+        public static Guid MyGuid { get; } = Guid.NewGuid();
+
+        static void Main(string[] args)
+        {
+            // Using Static directive
+            WriteLine("Hello! I'm Using Using Static directive feature of C# 6.0");            
+            
+            WriteLine(MyGuid);
+        }
+
+        
+    }
+
+    
+}
+    
---
+++ CSharp6.0UnitTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp6._0;
+
+namespace CSharp6._0UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UserCreatesID()
+        {
+            var one = new UserOne();
+            Assert.AreEqual(Guid.Empty, one.Id);
+        }
+
+    }
+}
