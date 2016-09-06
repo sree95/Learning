ba69959a408c64739b4af056e3c535905654ec72
sparimiEPAM <sparimi@HYD-SPARIMI01>
2016-09-06 20:32:37 +0530

    Added Fake repository and Unit testing

---
+++ PeopleViewer.Layered/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleViewer
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            repository = RepositoryFactory.GetRepository();
+        }
+
+        #region INotifyPropertyChangedMembers
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        private IPersonRepository repository;
+
+        private IEnumerable<Person> people;
+        public IEnumerable<Person> People
+        {
+            get
+            {
+                return people;
+            }
+            set
+            {
+                people = value;
+                RaisePropertyChanged("People");
+            }
+        }
+
+        public string RepositoryType
+        {
+            get
+            {
+                return repository.GetType().ToString();
+            }
+        }
+
+
+        public void FetchData()
+        {
+            repository.GetPeople();
+        }
+
+
+        public void ClearData()
+        {
+            people = new List<Person>();
+        }
+
+
+    }
+}
---
+++ PeopleViewer.Test/MainViewModelTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace PeopleViewer.Test
+{
+    [TestClass]
+    public class MainViewModelTest
+    {
+        [TestMethod]
+        public void People_OnFetchData_IsPopulated()
+        {
+            //Arrange
+
+            var viewModel = new MainViewModel();
+
+            //Act
+            viewModel.FetchData();
+
+            //Assert
+            Assert.IsNotNull(viewModel.People);
+            Assert.AreEqual(2, viewModel.People.Count());
+        }
+
+        [TestMethod]
+        public void RepositoryType_OnCreation_ReturnsFakeRepositoryString()
+        {
+            //Arrange // Act
+
+            var viewModel = new MainViewModel();
+            var expectedResult = "PersonRepository.Fake.FakeRepository";
+
+            // Assert
+
+            Assert.AreEqual(expectedResult, viewModel.RepositoryType);
+        }
+    }
+}
